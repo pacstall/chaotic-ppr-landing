90af35e7fe98794d1eba330ca54223a92eda4804
ajstrongdev <90976178+ajstrongdev@users.noreply.github.com>
2024-12-27 20:24:50 +0000

    Update src/pages/_document.js
    Co-authored-by: Elsie <hwengerstickel@protonmail.com>

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>Chaotic PPR | Pacstall</title>
-        <meta name="description" content="The (chaotic) Pacstall Package Repository" />
+        <meta name="description" content="The Chaotic Pacstall Package Repository" />
         <meta name="darkreader-lock" />
         <meta name="theme-color" content="#169390" />
         <link rel="icon" href="/pacstall.svg" />
